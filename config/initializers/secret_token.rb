03be28a3882d781bace53bb584ab32ebf6fa8a7b
Kim-Kjetil Grosland <kim.kjetil.grosland@gmail.com>
2012-10-13 10:34:41 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OiaApp::Application.config.secret_token = '4da638e3758fc3e05e2f59a554091430364e9d9b198ee46531a02e6aa1b152cb4bb4a675bf04d2cd1b3df8252b9bce2cb437696a5abf01e0177cf3f3f726a159'
