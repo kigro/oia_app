095420ec8901765a283d813fe938662eb3a72212
Kim-Kjetil Grosland <kim.kjetil.grosland@gmail.com>
2012-10-17 12:17:33 +0200

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(params[:user])
   	if @user.save
+      sign_in @user
   		flash[:success] = "Velkommen til Odd Ingars Golf App"
   		redirect_to @user
   	else
